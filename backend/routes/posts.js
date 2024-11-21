5ffb796297a0763c2af23d1a701cefa44286cef9
aGRAWAL <gargbhoomi01@gmail.com>
2024-11-21 18:51:57 +0530

    made some changes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const {getPosts, getPost, CreatePost, DeletePost, UpdatePost} = require('../controllers/PostsControllers')
-router.get('/',getPosts)
+router.get('/', (req, res) => {
+    console.log('GET /api/routes');
+    getPosts(req, res);
+});
 router.get('/:id',getPost)
 router.post('/',CreatePost)
 router.delete('/:id',DeletePost)
