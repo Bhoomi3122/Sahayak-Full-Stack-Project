f85329afa81b6a1b7cfa25364418504f72b0338c
aGRAWAL <gargbhoomi01@gmail.com>
2024-11-22 16:40:54 +0530

    made few changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'https://sahayak-nu.vercel.app/', // Replace with your frontend URL in production
+    origin: 'http://localhost:3000', // Replace with your frontend URL in production
     methods: 'GET,POST,PUT,DELETE', // Allow these HTTP methods
     allowedHeaders: 'Content-Type, Authorization' // Add more headers if needed
   }));
@@ -19,6 +19,10 @@ app.use((req,res,next)=>{
 app.use(express.json())
 
 app.use('/api/routes',postroutes)
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Route not found' });
+});
+
 const PORT = process.env.PORT || 4000
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
