aa3f735bc39649598cf0f193b4e5b7bdf3813a80
aGRAWAL <gargbhoomi01@gmail.com>
2024-11-22 17:28:52 +0530

    added frontend production path

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL in production
+    origin: 'https://sahayak-full-stack-project.onrender.com', // Replace with your frontend URL in production
     methods: 'GET,POST,PUT,DELETE', // Allow these HTTP methods
     allowedHeaders: 'Content-Type' // Add more headers if needed
   }));
